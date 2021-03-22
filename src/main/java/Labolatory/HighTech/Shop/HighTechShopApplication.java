f0c34db3e272f4595de4e6cfd7a7403eca45582c
PawelSzydlo1 <psz0587@gmail.com>
2021-03-22 23:58:58 +0100

    creating a project diagram. creating page views

---
+++ src/main/java/Labolatory/HighTech/Shop/HighTechShopApplication.java
@@ -0,0 +1,13 @@
+package Labolatory.HighTech.Shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HighTechShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HighTechShopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Labolatory/HighTech/Shop/HighTechShopApplicationTests.java
@@ -0,0 +1,13 @@
+package Labolatory.HighTech.Shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HighTechShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
